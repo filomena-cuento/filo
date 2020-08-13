f56c144c1bd647e36f5cbbda9c17b4ea0d96ef8f
Julian Gonzalez <juliangon25@gmail.com>
2020-08-13 02:33:35 -0300

    pag 3 lista

--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 export const ImageComponent = ({ props }) => {
 
-    const { className, url } = props; 
+    const { className, height, url } = props; 
 
     const finalClassName = className.concat(' animate__animated animate__fadeIn center-vert');
 
     return (
         <div className={ finalClassName }>
-            <img src={ process.env.PUBLIC_URL + url } alt="" width="auto" height="480"/>
+            <img src={ process.env.PUBLIC_URL + url } alt="" width="auto" height={height}/>
         </div>
     )
 
