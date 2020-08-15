dfb4a6eaef03253c37ff8b26ac1a9884aad3c258
julian-gh <juliangon25@gmail.com>
2020-08-15 00:28:57 -0300

    Ñ1 2 3 done

--- src/components/TextScreen.js
+++ src/components/TextScreen.js
@@ -5,6 +5,17 @@ export const TextScreen = () => {
 
     
     const FILOMENA = [
+        {   
+            id: 121,
+            type: "audio",
+            page: 0,
+            text: "",
+            className: "",
+            delay: 0, //animate__delay-1s
+            renderNext: true,
+            url: "/assets/sounds/whatAreTheRoots.mp3",
+            nextPage: false,
+        },
         {
             id: 0,
             type: "text",
@@ -282,17 +293,7 @@ export const TextScreen = () => {
             url: "",
             nextPage: false,
         },
-        {   
-            id: 121,
-            type: "audio",
-            page: 0,
-            text: "",
-            className: "",
-            delay: 0, //animate__delay-1s
-            renderNext: true,
-            url: "/assets/sounds/whatAreTheRoots.mp3",
-            nextPage: false,
-        },
+        
         {   
             id: 172,
             type: "text",
@@ -343,143 +344,3 @@ export const TextScreen = () => {
         </div>
     )
 }
-
-
-/*
-const FILOMENA =
-    [
-        {
-            id: 0,
-            type: "audio",
-            page: 0,
-            className: "",
-            delay: 0, 
-            renderNext: true,
-            url: "/assets/sounds/aprilIsTheCruelest.mp3",
-            nextPage: false,
-        },
-        {
-            id: 1,
-            type: "text",
-            page: 0,
-            text: "Abril es el mes más cruel. Puros deseos y recuerdos de cosas que dábamos por sentado unas semanas atrás. La lluvia de otoño rebota contra las veredas y es el único movimiento que hay allá afuera.",
-            className: "gcs-2 gce-4 grs-1 gre-2 center",
-            delay: 3500, //animate__delay-1s
-            renderNext: true,
-            url: "",
-            nextPage: false,
-        },
-        {   
-            id: 2,
-            type: "img",
-            page: 0,
-            text:"",
-            className: "gcs-1 gce-2 grs-1 gre-4",
-            // className: "img-left",
-            delay: 4000, //animate__delay-1s
-            renderNext: true,
-            url: "/assets/img/rata.png",
-            nextPage: false,
-        },
-        {
-            id: 3,
-            type: "text",
-            page: 0,
-            text: "Nos toca crecer en macetas de cemento, porque ya no se puede plantar los pies en la tierra.",
-            className: "gcs-2 gce-4 grs-2 center",
-            delay: 1500, //animate__delay-1s
-            renderNext: true,
-            url: "",
-            nextPage: false,
-        },
-        {   
-            id: 4,
-            type: "audio",
-            page: 0,
-            text: "",
-            className: "",
-            delay: 4000, //animate__delay-1s
-            renderNext: true,
-            url: "/assets/sounds/whatAreTheRoots.mp3",
-            nextPage: false,
-        },
-        {   
-            id: 5,
-            type: "text",
-            page: 0,
-            text:"¿Qué clase de plantas pueden nacer de estas cajas rígidas, sin poros, sin respiración? Ya solo nos queda una pila de imágenes rotas que guardamos en nuestras casas, a las que les damos forma para pasar el tiempo.",
-            className: "gcs-2 gce-4 grs-3 center",
-            delay: 6000, //animate__delay-1s
-            renderNext: false,
-            url: "",
-            nextPage: true,
-        },
-        {   
-            id: 6,
-            type: "audio",
-            page: 1,
-            text: "",
-            className: "",
-            delay: 4500, //animate__delay-1s
-            renderNext: true,
-            url: "/assets/sounds/speakToMe.mp3",
-            nextPage: false,
-        },
-        {
-            id: 7,
-            type: "text",
-            page: 1,
-            text: "Estás bestialmente quieto hoy.",
-            className: "gcs-1 gce-4 grs-1 gre-2",
-            delay: 2500, //animate__delay-1s
-            renderNext: true,
-            url: "",
-            nextPage: false,
-        },
-        {   
-            id: 8,
-            type: "text",
-            page: 1,
-            text:"Hablame.",
-            className: "gcs-3 grs-1",
-            delay: 2000, //animate__delay-1s
-            renderNext: true,
-            url: "",
-            nextPage: false,
-        },
-        {   
-            id: 9,
-            type: "audio",
-            page: 1,
-            text: "",
-            className: "",
-            delay: 500, //animate__delay-1s
-            renderNext: true,
-            url: "/assets/sounds/whatAreYouThinking.mp3",
-            nextPage: false,
-        },
-        {   
-            id: 10,
-            type: "text",
-            page: 1,
-            text:"¿Por qué no me hablás? ",
-            className: "gcs-1 gce-3 grs-2",
-            delay: 5000, //animate__delay-1s
-            renderNext: true,
-            url: "",
-            nextPage: false,
-        },
-        {   
-            id: 11,
-            type: "text",
-            page: 1,
-            text: "Nunca sé qué estás pensando. ¿Está vacía tu cabeza?",
-            className: "gcs-1 gce-4 grs-3",
-            delay: 3000, //animate__delay-1s
-            renderNext: false,
-            url: "",
-            nextPage: false,
-        },
-         
-    ]
-*/
