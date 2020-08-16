32c469d026b747f27096e2c43ef856e4bfd7504b
Julian Gonzalez <juliangon25@gmail.com>
2020-08-16 20:08:30 -0300

    added docs

---
+++ docs/precache-manifest.0d19208261225d2f1eac3a20faef93c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580fd4f6ee4b051ef32102470a628fb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c74ba8f11946f02d4599",
+    "url": "/static/css/2.3dbf44be.chunk.css"
+  },
+  {
+    "revision": "59c2bd2236e51d9f74e3",
+    "url": "/static/css/main.7190b2e8.chunk.css"
+  },
+  {
+    "revision": "c74ba8f11946f02d4599",
+    "url": "/static/js/2.38918a22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.38918a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59c2bd2236e51d9f74e3",
+    "url": "/static/js/main.c487a3d1.chunk.js"
+  },
+  {
+    "revision": "4e18b20bb1b213f86e8e",
+    "url": "/static/js/runtime-main.a0ad8f55.js"
+  },
+  {
+    "revision": "afab118368e0be026fe23b546ab2f878",
+    "url": "/static/media/room-bg2.afab1183.png"
+  }
+]);
