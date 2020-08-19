d4bb5bebb2d7b1feddf65a7653c628b62f31bf3a
Julian Gonzalez <juliangon25@gmail.com>
2020-08-19 20:39:13 -0300

    Updates

---
+++ precache-manifest.777186c43eb8c6af27203dc6b4aac6cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f233db80a8066f8e90a49e5dcdb1ef76",
+    "url": "/filo/index.html"
+  },
+  {
+    "revision": "3b4233c660915f34f67c",
+    "url": "/filo/static/css/2.3dbf44be.chunk.css"
+  },
+  {
+    "revision": "ece6082b3df79f78121a",
+    "url": "/filo/static/css/main.0300764f.chunk.css"
+  },
+  {
+    "revision": "3b4233c660915f34f67c",
+    "url": "/filo/static/js/2.0ee7ce85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/filo/static/js/2.0ee7ce85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece6082b3df79f78121a",
+    "url": "/filo/static/js/main.8ff595a1.chunk.js"
+  },
+  {
+    "revision": "eebc87187abdb3499a56",
+    "url": "/filo/static/js/runtime-main.0f8e9cd0.js"
+  },
+  {
+    "revision": "0784216ad4bbaa96b91ed97d229e46f4",
+    "url": "/filo/static/media/room-bg.0784216a.jpg"
+  }
+]);
